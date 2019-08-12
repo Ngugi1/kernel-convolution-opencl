4d817b4c487f90136bc9f8e13447f4a0880c6d23
Ngugi <samuel.ngugi@vub.be>
2019-08-12 14:03:24 +0200

    Added local memory implementation

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,5 +1,8 @@
 echo "Kernel Naive >>"
 PYOPENCL_COMPILER_OUTPUT=1 python main.py /Users/ngugi/Desktop/sonic.jpg kernel_naive
 
-echo "Kernel Uchar4"
+echo "Kernel Uchar4 >>"
 PYOPENCL_COMPILER_OUTPUT=1 python main.py /Users/ngugi/Desktop/sonic.jpg kernel_uchar4
+
+echo "Kernel Local Memory >>"
+PYOPENCL_COMPILER_OUTPUT=1 python main_local.py /Users/ngugi/Desktop/sonic.jpg kernel_uchar4_local_mem 16
---
+++ main_local.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+import pyopencl as cl
+import numpy
+import sys
+import cv2
+import time
+from util import *
+
+
+def conv(img_path, convolution_kernel, kernel_name, kernel_dim, kernel_mid, local_size):
+    # img - fully preprocessed image
+    # convolution_kernel - opencl kernel
+
+    # Load input image
+    img_no_padding = image_to_array(img_path)
+
+    # Add extra channel to enable
+    img_no_padding = add_alpha_channel(img_no_padding)
+
+    # Add padding to the image
+    padding = [0, 0, 0, 0]
+    img = cv2.copyMakeBorder(img_no_padding, kernel_mid, kernel_mid,
+                             kernel_mid, kernel_mid, cv2.BORDER_CONSTANT, value=padding)
+    img = pad_image(img, kernel_mid)
+
+    # Determine height and width of both original and padded image
+    (img_h, img_w, depth) = img.shape
+    (img_original_h, img_original_w, _) = img_no_padding.shape
+
+    # Flatten the image and the kernel
+    flat_img = img.reshape((img_h*img_w*depth))
+    flat_kernel = convolution_kernel.reshape((kernel_dim * kernel_dim))
+    # Create the result image.
+    h_output_img = numpy.zeros(img_h * img_w * depth).astype(numpy.uint8)
+
+    # opencl kernel
+    kernel = open(kernel_name + ".cl").read()
+    # Create context
+    # Choose a device
+    platforms = cl.get_platforms()
+    devices = platforms[0].get_devices()
+    context = cl.Context([devices[1]])
+
+
+# Or choose a device manually
+# context = cl.create_some_context()
+
+# Create a queue to the device.
+    queue = cl.CommandQueue(context)
+
+    # Create the program.
+    program = cl.Program(context, kernel).build()
+
+    # Send the data to the guest memory.
+    d_input_img = cl.Buffer(context, cl.mem_flags.READ_ONLY |
+                            cl.mem_flags.COPY_HOST_PTR, hostbuf=flat_img)
+    d_kernel = cl.Buffer(context, cl.mem_flags.READ_ONLY |
+                         cl.mem_flags.COPY_HOST_PTR, hostbuf=flat_kernel)
+
+    # Create the memory on the device to put the result into.
+    d_output_img = cl.Buffer(
+        context, cl.mem_flags.WRITE_ONLY, h_output_img.nbytes)
+
+    conv = program.convolve
+    conv.set_scalar_arg_dtypes(
+        [None, None, None, numpy.uint8, numpy.uint8, numpy.uint32, numpy.uint32, numpy.uint8, None])
+    global_size = (img_original_h, img_original_w)
+    l = 16
+    workgroup_size = (l * l * depth)
+    # workgroup_size = (local_size, local_size)
+    local_memory = cl.LocalMemory(numpy.dtype(
+        numpy.uint8).itemsize * workgroup_size)
+
+    for i in range(33):
+        x = 10
+        total_time = 0
+        while(x > 0):
+            # Execute the kernel
+            start_time = time.time()
+            conv(queue, global_size, (l, l), d_input_img,
+                 d_output_img, d_kernel, kernel_dimensions, kernel_mid, img_w, img_h, depth, local_memory)
+            # Wait for the queue to be completely processed.
+            queue.finish()
+            # Read the array from the device.
+            cl.enqueue_copy(queue, h_output_img, d_output_img)
+            queue.finish()
+            end_time = time.time()
+            total_time += end_time - start_time
+            x -= 1
+        # stop the time
+        print((total_time / 10) * 1000)
+
+    # Reshape the image array
+    result_image = h_output_img.reshape(img.shape)
+
+    # Now remove the padding from image
+    result_image = result_image[kernel_mid:-kernel_mid]
+
+    for i in range(len(result_image)):
+        img_no_padding[i] = result_image[i][kernel_mid:-kernel_mid]
+
+    # final img
+    res = numpy.asarray(img_no_padding).astype(dtype=numpy.uint8)
+
+    # Return the time and the image
+    return res
+
+
+# Execute
+# Gausian kernel
+# Convolution kernel
+kernel_dimensions = 5
+kernel_sign = 1
+kernel_mid = kernel_dimensions / 2
+convolution_kernel = gaussian_kernel(kernel_dimensions, kernel_sign)
+kernel_name = sys.argv[2]
+local_size = sys.argv[3]
+
+image = conv(
+    sys.argv[1], convolution_kernel, kernel_name, kernel_dimensions, kernel_mid, local_size)
+save_image(image, "output_" + kernel_name + ".jpg")
