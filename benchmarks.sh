5daf23b7051690ffb97e31f2c003b973f25a10e2
Ngugi <samuel.ngugi@vub.be>
2019-08-17 17:25:13 +0200

    benchmarks

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 work_group_size=8
-kernel_dim=5
+kernel_dim=9
 img_path="./input/img1.jpg"
 # change kernel_dimensions
 
@@ -71,9 +71,9 @@ img_path="./input/img1.jpg"
 # python main_local.py $img_path kernel_uchar4_local_mem $work_group_size 13 1 >> ./output_gpu_k_dim/output_gpu_local_mem_kernel_dim_13.csv
 # python main_local.py $img_path kernel_uchar4_local_mem $work_group_size 17 1 >> ./output_gpu_k_dim/output_gpu_local_mem_kernel_dim_17.csv
 # python main_local.py $img_path kernel_uchar4_local_mem 4 $kernel_dim 1 >> ./output_gpu_wgsize/output_gpu_local_mem_wgs_4.csv
-# python main_local.py $img_path kernel_uchar4_local_mem 32 $kernel_dim 1 >> ./output_gpu_wgsize/output_gpu_local_mem_wgs_32.csv
+python main_local.py $img_path kernel_uchar4_local_mem 32 $kernel_dim 1 >> ./output_gpu_wgsize/output_gpu_local_mem_wgs_32.csv
 
 
-echo "Sequential K-dim-"$kernel_dim
-python sequential.py $img_path $kernel_dim >> ./baseline.csv
+# echo "Sequential K-dim-"$kernel_dim
+# python sequential.py $img_path $kernel_dim >> ./baseline.csv
 
